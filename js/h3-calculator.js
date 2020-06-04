4a23600091df4992b3ae6ebb370b33637f9454ff
Choldor <choldor123@gmail.com>
2020-06-04 12:05:58 +0200

    poczatek calculatora

---
+++ js/h3-calculator.js
@@ -0,0 +1,16 @@
+const spells = [
+    {name:"Magic Arrow", power:10, power1:10, power2:20, power3:30},
+    {name:"Fire Wall", power:10, power1:10, power2:20, power3:30},
+    {name:"Fire Ball", power:10, power1:15, power2:30, power3:60},
+    {name:"Land Mine", power:10, power1:25, power2:50, power3:100},
+    {name:"Armageddon", power:50, power1:30, power2:60, power3:120},
+    {name:"Inferno", power:10, power1:25, power2:40, power3:80},
+    {name:"Lightning Bolt", power:25, power1:10, power2:20, power3:30},
+    {name:"Chain Lightning", power:40, power1:25, power2:50, power3:100},
+    {name:"Ice Bolt", power:20, power1:10, power2:20, power3:30},
+    {name:"Frost Ring", power:10, power1:15, power2:30, power3:60},
+    {name:"Death Ripple", power:5, power1:10, power2:20, power3:30},
+    {name:"Meteor Shower", power:25, power1:25, power2:50, power3:100},
+    {name:"Implosion", power:75, power1:100, power2:200, power3:300},
+]
+console.log(spells)
