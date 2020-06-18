6f2f7d0d73dc4f3be1baea2e38e284ab7c876e9e
Choldor <choldor123@gmail.com>
2020-06-18 12:23:13 +0200

    zamki i jednoski

--- js/h3-calculator.js
+++ js/h3-calculator.js
@@ -68,27 +68,168 @@ const showFire = () => {
   fire.forEach((fire) => (fire.style.display = "inline-flex"));
 };
 magicFire.addEventListener("click", showFire);
+
 const showAir = () => {
-  allSpells.forEach(singleSpell => singleSpell.style.display = "none");
-  air.forEach(air => air.style.display = "inline-flex");
+  allSpells.forEach((singleSpell) => (singleSpell.style.display = "none"));
+  air.forEach((air) => (air.style.display = "inline-flex"));
 };
 magicAir.addEventListener("click", showAir);
+
 const showWater = () => {
   allSpells.forEach((singleSpell) => (singleSpell.style.display = "none"));
   water.forEach((water) => (water.style.display = "inline-flex"));
 };
 magicWater.addEventListener("click", showWater);
+
 const showEarth = () => {
   allSpells.forEach((singleSpell) => (singleSpell.style.display = "none"));
   earth.forEach((earth) => (earth.style.display = "inline-flex"));
 };
 magicEarth.addEventListener("click", showEarth);
+
 const showAll = () => {
-  allSpells.forEach((singleSpell) => (singleSpell.style.display = "inline-flex"));
+  allSpells.forEach(
+    (singleSpell) => (singleSpell.style.display = "inline-flex")
+  );
 };
 magicAll.addEventListener("click", showAll);
 
-allSpells.forEach(spell => spell.addEventListener("click", () => {
-  
-  console.log(spell.dataset.spellName)
-}) ) 
+
+allSpells.forEach((spell) =>
+  spell.addEventListener("click", () => {
+    console.log(spell.dataset.spellName);
+  })
+);
+
+const allCreatures = document.querySelectorAll(".img-creature");
+const castleCreatures = document.querySelectorAll(".castle-creature");
+const rampartCreatures = document.querySelectorAll(".rampart-creature")
+const towerCreatures = document.querySelectorAll(".tower-creature")
+const infernoCreatures = document.querySelectorAll(".inferno-creature")
+const necropolisCreatures = document.querySelectorAll(".necropolis-creature")
+const dungeonCreatures = document.querySelectorAll(".dungeon-creature")
+const strongholdCreatures = document.querySelectorAll(".stronghold-creature")
+const fortressCreatures = document.querySelectorAll(".fortress-creature")
+const confluxCreatures = document.querySelectorAll(".conflux-creature")
+const coveCreatures = document.querySelectorAll(".cove-creature")
+const neutralCreatures = document.querySelectorAll(".neutral-creature")
+const townCastle = document.querySelector(".castle");
+const townRampart = document.querySelector(".rampart");
+const townTower = document.querySelector(".tower");
+const townInferno = document.querySelector(".inferno");
+const townNecropolis = document.querySelector(".necropolis");
+const townDungeon = document.querySelector(".dungeon");
+const townStronghold = document.querySelector(".stronghold");
+const townFortress = document.querySelector(".fortress");
+const townConflux = document.querySelector(".conflux");
+const townCove = document.querySelector(".cove");
+const townNeutral = document.querySelector(".neutral");
+const showCastle = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  castleCreatures.forEach(
+    (castleCreatures) => (castleCreatures.style.display = "inline-flex")
+  );
+};
+townCastle.addEventListener("click", showCastle);
+
+const showRampart = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  rampartCreatures.forEach(
+    (rampartCreatures) => (rampartCreatures.style.display = "inline-flex")
+  );
+};
+townRampart.addEventListener("click", showRampart);
+
+const showTower = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  towerCreatures.forEach(
+    (towerCreatures) => (towerCreatures.style.display = "inline-flex")
+  );
+};
+townTower.addEventListener("click", showTower);
+
+const showInferno = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  infernoCreatures.forEach(
+    (infernoCreatures) => (infernoCreatures.style.display = "inline-flex")
+  );
+};
+townInferno.addEventListener("click", showInferno);
+
+const showNecropolis = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  necropolisCreatures.forEach(
+    (necropolisCreatures) => (necropolisCreatures.style.display = "inline-flex")
+  );
+};
+townNecropolis.addEventListener("click", showNecropolis);
+
+const showDungeon = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  dungeonCreatures.forEach(
+    (dungeonCreatures) => (dungeonCreatures.style.display = "inline-flex")
+  );
+};
+townDungeon.addEventListener("click", showDungeon);
+
+const showStronghold = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  strongholdCreatures.forEach(
+    (strongholdCreatures) => (strongholdCreatures.style.display = "inline-flex")
+  );
+};
+townStronghold.addEventListener("click", showStronghold);
+
+const showFortress = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  fortressCreatures.forEach(
+    (fortressCreatures) => (fortressCreatures.style.display = "inline-flex")
+  );
+};
+townFortress.addEventListener("click", showFortress);
+
+const showConflux = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  confluxCreatures.forEach(
+    (confluxCreatures) => (confluxCreatures.style.display = "inline-flex")
+  );
+};
+townConflux.addEventListener("click", showConflux);
+
+const showCove = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  coveCreatures.forEach(
+    (coveCreatures) => (coveCreatures.style.display = "inline-flex")
+  );
+};
+townCove.addEventListener("click", showCove);
+
+const showNeutral = () => {
+  allCreatures.forEach(
+    (singleCreature) => (singleCreature.style.display = "none")
+  );
+  neutralCreatures.forEach(
+    (neutralCreatures) => (neutralCreatures.style.display = "inline-flex")
+  );
+};
+townNeutral.addEventListener("click", showNeutral);
